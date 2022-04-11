a82307a9df4ef88810523d660ae70a82fd46dd56
Dimitris Mamakas <dimitrisaxo55@gmail.com>
2022-04-11 17:14:57 +0300

    Initial commit

---
+++ src/main/java/gr/aueb/hci/HumanComputerInteractionApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.hci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanComputerInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanComputerInteractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/hci/HumanComputerInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.hci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanComputerInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
