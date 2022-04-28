b4df0262375ab1fc66575fd803baddecd7f2d8c4
Dimitris Mamakas <dimitrisaxo55@gmail.com>
2022-04-28 22:01:41 +0300

    Beautified CoolingAlert.java

--- src/main/java/gr/aueb/hci/alerts/CoolingAlert.java
+++ src/main/java/gr/aueb/hci/alerts/CoolingAlert.java
@@ -13,7 +13,50 @@ import javax.swing.border.EmptyBorder;
 
 public class CoolingAlert extends JFrame {
 
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = -6648662630435458748L;
+
+    /**
+     * Icon.
+     */
+    private ImageIcon snowFlakeIcon = new ImageIcon( "assets/images/cool/snowflake_icon_big.png" );
+
+    /**
+     * Content pane.
+     */
     private JPanel contentPane;
+
+    /**
+     * Default constructor.
+     */
+    public CoolingAlert() {
+        setResizable( false );
+        setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        setBounds( 100, 100, 806, 660 );
+        this.contentPane = new JPanel();
+        this.contentPane.setBackground( Color.WHITE );
+        this.contentPane.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
+        setContentPane( this.contentPane );
+        this.contentPane.setLayout( null );
+
+        final JLabel textMessageLabel = new JLabel(
+                "\u0397 \u03BB\u03B5\u03B9\u03C4\u03BF\u03C5\u03C1\u03B3\u03AF\u03B1 \u03C8\u03CD\u03BE\u03B7\u03C2 \u03B5\u03BD\u03B5\u03C1\u03B3\u03BF\u03C0\u03BF\u03B9\u03AE\u03B8\u03B7\u03BA\u03B5" );
+        textMessageLabel.setFont( new Font( "Arial", Font.PLAIN, 40 ) );
+        textMessageLabel.setBounds( 69, 78, 650, 69 );
+        this.contentPane.add( textMessageLabel );
+
+        final JLabel textMessageLabel1 = new JLabel( "\u03B5\u03C0\u03B9\u03C4\u03C5\u03C7\u03CE\u03C2!" );
+        textMessageLabel1.setFont( new Font( "Arial", Font.PLAIN, 40 ) );
+        textMessageLabel1.setBounds( 296, 135, 192, 47 );
+        this.contentPane.add( textMessageLabel1 );
+
+        final JLabel iconLabel = new JLabel( "" );
+        iconLabel.setBounds( 296, 252, 248, 202 );
+        iconLabel.setIcon( this.snowFlakeIcon );
+        this.contentPane.add( iconLabel );
+    }
 
     /**
      * Launch the application.
@@ -32,35 +75,4 @@ public class CoolingAlert extends JFrame {
             }
         } );
     }
-
-    /**
-     * Create the frame.
-     */
-    public CoolingAlert() {
-        setResizable( false );
-        setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-        setBounds( 100, 100, 806, 660 );
-        this.contentPane = new JPanel();
-        this.contentPane.setBackground( Color.WHITE );
-        this.contentPane.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
-        setContentPane( this.contentPane );
-        this.contentPane.setLayout( null );
-
-        final JLabel lblNewLabel = new JLabel(
-                "\u0397 \u03BB\u03B5\u03B9\u03C4\u03BF\u03C5\u03C1\u03B3\u03AF\u03B1 \u03C8\u03CD\u03BE\u03B7\u03C2 \u03B5\u03BD\u03B5\u03C1\u03B3\u03BF\u03C0\u03BF\u03B9\u03AE\u03B8\u03B7\u03BA\u03B5" );
-        lblNewLabel.setFont( new Font( "Arial", Font.PLAIN, 40 ) );
-        lblNewLabel.setBounds( 69, 78, 650, 69 );
-        this.contentPane.add( lblNewLabel );
-
-        final JLabel lblNewLabel_1 = new JLabel( "\u03B5\u03C0\u03B9\u03C4\u03C5\u03C7\u03CE\u03C2!" );
-        lblNewLabel_1.setFont( new Font( "Arial", Font.PLAIN, 40 ) );
-        lblNewLabel_1.setBounds( 296, 135, 192, 47 );
-        this.contentPane.add( lblNewLabel_1 );
-
-        final JLabel lblNewLabel_2 = new JLabel( "" );
-        lblNewLabel_2.setBounds( 296, 252, 248, 202 );
-        lblNewLabel_2.setIcon( new ImageIcon( "snowflake2.png" ) );
-        this.contentPane.add( lblNewLabel_2 );
-    }
-
 }
