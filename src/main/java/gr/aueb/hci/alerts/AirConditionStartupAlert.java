307d41b52f219cf4dac06fedf1e5a65c01ed6be9
Dimitris Mamakas <dimitrisaxo55@gmail.com>
2022-04-28 21:55:29 +0300

    Beautified AirConditionStartupAlert.java

--- src/main/java/gr/aueb/hci/alerts/AirConditionStartupAlert.java
+++ src/main/java/gr/aueb/hci/alerts/AirConditionStartupAlert.java
@@ -13,7 +13,51 @@ import javax.swing.border.EmptyBorder;
 
 public class AirConditionStartupAlert extends JFrame {
 
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = -2371099651394848321L;
+
+    /**
+     * Icon.
+     */
+    private ImageIcon verificationIcon = new ImageIcon( "assets/images/verification/verification_icon.png" );
+
+    /**
+     * Content pane.
+     */
     private JPanel contentPane;
+
+    /**
+     * Default constructor.
+     */
+    public AirConditionStartupAlert() {
+        setBackground( Color.WHITE );
+        setResizable( false );
+        setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        setBounds( 100, 100, 806, 660 );
+        this.contentPane = new JPanel();
+        this.contentPane.setBackground( Color.WHITE );
+        this.contentPane.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
+        setContentPane( this.contentPane );
+        this.contentPane.setLayout( null );
+
+        final JLabel verificationMessageLabel = new JLabel(
+                "\u03A4\u03BF \u03BA\u03BB\u03B9\u03BC\u03B1\u03C4\u03B9\u03C3\u03C4\u03B9\u03BA\u03CC \u03B5\u03BA\u03BA\u03AF\u03BD\u03B7\u03C3\u03B5 " );
+        verificationMessageLabel.setFont( new Font( "Arial", Font.PLAIN, 50 ) );
+        verificationMessageLabel.setBounds( 111, 55, 635, 125 );
+        this.contentPane.add( verificationMessageLabel );
+
+        final JLabel verificationMessageLabel1 = new JLabel( "\u03B5\u03C0\u03B9\u03C4\u03C5\u03C7\u03CE\u03C2!" );
+        verificationMessageLabel1.setFont( new Font( "Arial", Font.PLAIN, 50 ) );
+        verificationMessageLabel1.setBounds( 285, 143, 250, 68 );
+        this.contentPane.add( verificationMessageLabel1 );
+
+        final JLabel verificationIconLabel = new JLabel( "" );
+        verificationIconLabel.setBounds( 301, 258, 262, 213 );
+        verificationIconLabel.setIcon( this.verificationIcon );
+        this.contentPane.add( verificationIconLabel );
+    }
 
     /**
      * Launch the application.
@@ -31,36 +75,5 @@ public class AirConditionStartupAlert extends JFrame {
                 }
             }
         } );
-    }
-
-    /**
-     * Create the frame.
-     */
-    public AirConditionStartupAlert() {
-        setBackground( Color.WHITE );
-        setResizable( false );
-        setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-        setBounds( 100, 100, 806, 660 );
-        this.contentPane = new JPanel();
-        this.contentPane.setBackground( Color.WHITE );
-        this.contentPane.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
-        setContentPane( this.contentPane );
-        this.contentPane.setLayout( null );
-
-        final JLabel lblNewLabel = new JLabel(
-                "\u03A4\u03BF \u03BA\u03BB\u03B9\u03BC\u03B1\u03C4\u03B9\u03C3\u03C4\u03B9\u03BA\u03CC \u03B5\u03BA\u03BA\u03AF\u03BD\u03B7\u03C3\u03B5 " );
-        lblNewLabel.setFont( new Font( "Arial", Font.PLAIN, 50 ) );
-        lblNewLabel.setBounds( 111, 55, 635, 125 );
-        this.contentPane.add( lblNewLabel );
-
-        final JLabel lblNewLabel_1 = new JLabel( "\u03B5\u03C0\u03B9\u03C4\u03C5\u03C7\u03CE\u03C2!" );
-        lblNewLabel_1.setFont( new Font( "Arial", Font.PLAIN, 50 ) );
-        lblNewLabel_1.setBounds( 285, 143, 250, 68 );
-        this.contentPane.add( lblNewLabel_1 );
-
-        final JLabel lblNewLabel_2 = new JLabel( "" );
-        lblNewLabel_2.setBounds( 301, 258, 262, 213 );
-        lblNewLabel_2.setIcon( new ImageIcon( "verification1.png" ) );
-        this.contentPane.add( lblNewLabel_2 );
     }
 }
