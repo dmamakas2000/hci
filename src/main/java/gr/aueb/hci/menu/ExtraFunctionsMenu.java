734b75a98482f13a63b701869bceff33bc3c1454
Dimitris Mamakas <dimitrisaxo55@gmail.com>
2022-05-16 12:38:19 +0300

    Fixing ExtraFunctionsMenu UI

--- src/main/java/gr/aueb/hci/menu/ExtraFunctionsMenu.java
+++ src/main/java/gr/aueb/hci/menu/ExtraFunctionsMenu.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -21,41 +22,36 @@ import gr.aueb.hci.singleton.Singleton;
 public class ExtraFunctionsMenu extends JFrame {
 
     /**
-     *
+     * Generated version UID.
      */
     private static final long serialVersionUID = 4096472432248884379L;
 
+    /**
+     * Content pane.
+     */
     private JPanel contentPane;
 
+    /**
+     * Help button.
+     */
     private JButton needHelpButton;
 
+    /**
+     * Icons.
+     */
     private ImageIcon needHelpIconForButton = new ImageIcon( "assets/images/help/info.png" );
 
-    private static final String HELP_MESSAGE = "";
-
     /**
-     * Launch the application.
+     * Help message.
      */
-    public static void main( final String[] args ) {
-        EventQueue.invokeLater( new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    final ExtraFunctionsMenu frame = new ExtraFunctionsMenu();
-                    frame.setVisible( true );
-                }
-                catch ( final Exception e ) {
-                    e.printStackTrace();
-                }
-            }
-        } );
-    }
+    private static final String HELP_MESSAGE = "Επιλέξτε μία από τις παρακάτω επιπλέον λειτουργίες για καλύτερα αποτελέσματα.";
 
     /**
-     * Create the frame.
+     * Default constructor.
      */
     public ExtraFunctionsMenu() {
         setBackground( Color.WHITE );
+        setIconImage( Toolkit.getDefaultToolkit().getImage( Singleton.getInstance().getFrameIcon() ) );
         setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
         setBounds( 100, 100, Singleton.getInstance().getWindowSizeWidth(), Singleton.getInstance().getWindowSizeHeight() );
         this.contentPane = new JPanel();
@@ -64,37 +60,49 @@ public class ExtraFunctionsMenu extends JFrame {
         setContentPane( this.contentPane );
         this.contentPane.setLayout( null );
 
-        final JLabel lblNewLabel = new JLabel(
+        final JLabel mainLabel1 = new JLabel(
                 "\u039C\u03C0\u03BF\u03C1\u03B5\u03AF\u03C4\u03B5 \u03BD\u03B1 \u03B5\u03C0\u03B9\u03BB\u03AD\u03BE\u03B5\u03C4\u03B5 \u03BC\u03AF\u03B1 \u03B1\u03C0\u03CC \u03C4\u03B9\u03C2" );
-        lblNewLabel.setFont( new Font( "Arial", Font.PLAIN, 30 ) );
-        lblNewLabel.setBounds( 162, 35, 491, 51 );
-        this.contentPane.add( lblNewLabel );
+        mainLabel1.setFont( new Font( "Arial", Font.PLAIN, 30 ) );
+        mainLabel1.setBounds( 162, 35, 491, 51 );
+        this.contentPane.add( mainLabel1 );
 
-        final JLabel lblNewLabel_1 = new JLabel(
+        final JLabel mainLabel2 = new JLabel(
                 "\u03C0\u03B1\u03C1\u03B1\u03BA\u03AC\u03C4\u03C9 \u03B4\u03B9\u03B1\u03B8\u03AD\u03C3\u03B9\u03BC\u03B5\u03C2 \u03BB\u03B5\u03B9\u03C4\u03BF\u03C5\u03C1\u03B3\u03AF\u03B5\u03C2" );
-        lblNewLabel_1.setFont( new Font( "Arial", Font.PLAIN, 30 ) );
-        lblNewLabel_1.setBounds( 162, 81, 456, 58 );
-        this.contentPane.add( lblNewLabel_1 );
+        mainLabel2.setFont( new Font( "Arial", Font.PLAIN, 30 ) );
+        mainLabel2.setBounds( 162, 81, 456, 58 );
+        this.contentPane.add( mainLabel2 );
 
         final JButton inverterMode = new JButton(
                 "\u039B\u03B5\u03B9\u03C4\u03BF\u03C5\u03C1\u03B3\u03AF\u03B1 \u03B1\u03C6\u03CD\u03B3\u03C1\u03B1\u03BD\u03C3\u03B7\u03C2" );
+        inverterMode.setBackground( Color.WHITE );
         inverterMode.setFont( new Font( "Arial", Font.PLAIN, 25 ) );
+        inverterMode.setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
+        inverterMode.setFocusPainted( false );
         inverterMode.setBounds( 31, 266, 332, 65 );
         this.contentPane.add( inverterMode );
 
         final JButton turboMode = new JButton( "\u039B\u03B5\u03B9\u03C4\u03BF\u03C5\u03C1\u03B3\u03AF\u03B1 TURBO" );
+        turboMode.setBackground( Color.WHITE );
         turboMode.setFont( new Font( "Arial", Font.PLAIN, 25 ) );
+        turboMode.setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
+        turboMode.setFocusPainted( false );
         turboMode.setBounds( 31, 368, 332, 65 );
         this.contentPane.add( turboMode );
 
         final JButton economyMode = new JButton(
                 "\u039B\u03B5\u03B9\u03C4\u03BF\u03C5\u03C1\u03B3\u03AF\u03B1 \u03BF\u03B9\u03BA\u03BF\u03BD\u03BF\u03BC\u03AF\u03B1\u03C2" );
+        economyMode.setBackground( Color.WHITE );
         economyMode.setFont( new Font( "Arial", Font.PLAIN, 25 ) );
+        economyMode.setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
+        economyMode.setFocusPainted( false );
         economyMode.setBounds( 450, 266, 332, 65 );
         this.contentPane.add( economyMode );
 
         final JButton goBackButton = new JButton( "\u03A0\u03AE\u03B3\u03B1\u03B9\u03BD\u03B5 \u03BC\u03B5 \u03C0\u03AF\u03C3\u03C9" );
+        goBackButton.setBackground( Color.WHITE );
         goBackButton.setFont( new Font( "Arial", Font.PLAIN, 25 ) );
+        goBackButton.setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
+        goBackButton.setFocusPainted( false );
         goBackButton.setBounds( 31, 533, 332, 65 );
         goBackButton.addActionListener( new ActionListener() {
             @Override
@@ -116,14 +124,32 @@ public class ExtraFunctionsMenu extends JFrame {
         this.needHelpButton.addActionListener( new ActionListener() {
             @Override
             public void actionPerformed( final ActionEvent e ) {
-                JOptionPane.showMessageDialog( null, ExtraFunctionsMenu.HELP_MESSAGE, "Κεντρικό μενού", 1 );
+                JOptionPane.showMessageDialog( null, ExtraFunctionsMenu.HELP_MESSAGE, "Μενού πρόσθετων επιλογών", 1 );
             }
         } );
         this.contentPane.add( this.needHelpButton );
 
-        final JLabel lblNewLabel_2 = new JLabel( "\u0398\u03AD\u03BB\u03C9 \u03B2\u03BF\u03AE\u03B8\u03B5\u03B9\u03B1" );
-        lblNewLabel_2.setFont( new Font( "Arial", Font.PLAIN, 25 ) );
-        lblNewLabel_2.setBounds( 524, 553, 203, 43 );
-        this.contentPane.add( lblNewLabel_2 );
+        final JLabel needHelpLabel = new JLabel( "\u0398\u03AD\u03BB\u03C9 \u03B2\u03BF\u03AE\u03B8\u03B5\u03B9\u03B1" );
+        needHelpLabel.setFont( new Font( "Arial", Font.PLAIN, 25 ) );
+        needHelpLabel.setBounds( 524, 553, 203, 43 );
+        this.contentPane.add( needHelpLabel );
+    }
+
+    /**
+     * Launch the application.
+     */
+    public static void main( final String[] args ) {
+        EventQueue.invokeLater( new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final ExtraFunctionsMenu frame = new ExtraFunctionsMenu();
+                    frame.setVisible( true );
+                }
+                catch ( final Exception e ) {
+                    e.printStackTrace();
+                }
+            }
+        } );
     }
 }
